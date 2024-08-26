c550cb8782fa9dde7b182dbb739e5d7bb02245d8
Jacky Conant <a01280544@tec.mx>
2024-08-26 10:13:45 -0600

    cuarto push

--- proyecto.py
+++ proyecto.py
@@ -1,13 +1,17 @@
 #Importar librerías
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import math
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+#Importar algoritmo de regresión 
+from regression import *
 
 #Inicializar la base de datos a utilizar
-lol = pd.read_csv('lol_champ.csv')
+lol = pd.read_csv('global_population_growth_2024.csv')
 print(lol.head())
-lol.plot()
+#lol.plot()
 lol.plot(kind = 'scatter', x = 'Attack damage per lvl', y = 'AS ratio')
-
 plt.show()
-#print(plot)
-
